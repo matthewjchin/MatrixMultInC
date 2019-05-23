dedca700482855a3c1e5201537f3c5e558637386
matthewjchin <mattchin813@gmail.com>
2019-05-22 23:17:24 -0700

    Updates 5/22

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
+MatrixMultInC
+---------------------
+
 File name: matrix.c <br>
 Creator: Matthew Chin (matthewjchin) <br>
 
 Description: 
-A program written in C that takes a 3x4 and a 4x2 matrix that does matrix multiplication. 
+A program written in C that takes a 3x4 and a 4x2 matrix that does matrix multiplication.
+
+The result of matrix multiplication for the functionality done in matrix.c is calculated
+to be a 3x2 matrix as a result of the number of columns for the first matrix matching the
+same value of the number of rows for the second matrix. 
 
 In future: Will allow for two different matrices of different dimensions, without limits. 
+Also: Check for invalid dimenstions of matrices when multiplying as well as matrix addition. 
 
 Changes to Repo:
 --------------------
 4 May 2019: First push. 
+7 May 2019: Aditional Details; formatting header of README file.
+22 May 2019: Added comments to matrix.c file, added to README file.
--- matrix.c
+++ matrix.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/*
+    Asks the user for input by inserting elements in the matrix
+    Returns the matrix by dimensions as entered by the user in that order
+*/
 void create(int x, int y, int arr[x][y])
 {
     printf("Please insert elements into the matrix:\n");
@@ -18,6 +22,13 @@ void create(int x, int y, int arr[x][y])
     }
 }
 
+/*
+    Display the matrix that is inputted by the user. 
+
+    Does the calculations for each value and does matrix multiplication
+    that results in a 3 x 2 Matrix after all the calculations have 
+    been done. 
+ */
 void display(int x, int y, int z, int a1[y][z], int a2[z][x], int na[y][x])
 {
     for(int a=0; a<y; a++){
@@ -38,13 +49,15 @@ int main()
     int prod_arr[b][a];
 
     printf("First Matrix has dimensions of 3 x 4.\n");
-    create(b, c, arr1);
+    create(b, c, arr1); // asks the user to input the values (12 total) for 3x4
     printf("Second Matrix has dimensions of 4 x 2.\n");
-    create(c, a, arr2);
+    create(c, a, arr2); // asks user to input the values (8 total) for 4x2
 
     printf("\n");
     printf("The product of the two arrays:\n");
-    display(a, b, c, arr1, arr2, prod_arr);
+    
+    // Matrix multiplication calculations function called
+    display(a, b, c, arr1, arr2, prod_arr); 
 
     for(int i=0; i<b; i++){
         for(int j=0; j<a; j++)
