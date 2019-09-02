38541e23c86aafb72fa4c94f8455241a5cc0143f
matthewjchin <mattchin813@gmail.com>
2019-09-01 20:03:01 -0700

    Added file for different matrix dimensions.

---
+++ new_matrix.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("Enter dimensions of first matrix: \n");
+
+
+    printf("Enter dimensions of second matrix: \n");    
+    return 0;
+}
